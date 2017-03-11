5d620a273a198d773d6c5eedcc3e1f6c675d4304
sinicin <sinicin@bolid.ru>
2017-03-11 17:28:32 +0300

    Нотификация, а так же нужно написать службу

--- uMain.pas
+++ uMain.pas
@@ -17,7 +17,7 @@ uses
   System.Rtti, FMX.Bind.Grid, System.Bindings.Outputs, FMX.Bind.Editors,
   Data.Bind.Controls, Data.Bind.EngExt, FMX.Bind.DBEngExt, Data.Bind.Components,
   FMX.Bind.Navigator, Data.Bind.Grid, FMX.Grid, Data.Bind.DBScope,
-  FireDAC.Comp.DataSet, Engine, FMX.Edit, FMX.ScrollBox;
+  FireDAC.Comp.DataSet, Engine, FMX.Edit, FMX.ScrollBox, System.Notification;
 
 type
   TMainForm = class(TForm)
@@ -43,6 +43,7 @@ type
     ListBoxItem2: TListBoxItem;
     ListBoxItem3: TListBoxItem;
     ListBoxItem4: TListBoxItem;
+    NotificationCenter: TNotificationCenter;
     procedure ListBoxItemTargetClick(Sender: TObject);
     procedure FormShow(Sender: TObject);
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
@@ -51,6 +52,7 @@ type
     { Private declarations }
   public
     { Public declarations }
+    procedure ShowTestNotification;
   end;
 
 var
@@ -72,10 +74,10 @@ end;
 procedure TMainForm.FormShow(Sender: TObject);
 begin
 {$IFDEF ANDROID}
+  ShowTestNotification;
   FDConnection.Params.Values['Database'] := '$(DOC)/database.sqlite';
 {$ELSE}
-  FDConnection.Params.Values['Database'] := ExtractFilePath(ParamStr(0)) +
-    'database.sqlite';
+  FDConnection.Params.Values['Database'] := ExtractFilePath(ParamStr(0)) + 'database.sqlite';
 {$ENDIF}
   FDConnection.DriverName := 'SQLite';
   FDConnection.Connected := True;
@@ -92,6 +94,21 @@ procedure TMainForm.ListBoxItemTargetClick(Sender: TObject);
 begin
   FormTaskList.Show;
   MultiView.HideMaster;
+end;
+
+procedure TMainForm.ShowTestNotification;
+var
+  Notification: TNotification;
+begin
+  Notification := NotificationCenter.CreateNotification; // создаем экземпляр класса TNotification
+  try // обработчик ошибок
+    Notification.Name := 'MyNotification'; // Название
+    Notification.AlertBody := 'Уведомление: PWcode.net'; // содержание уведомления
+    Notification.FireDate := Now + EncodeTime(0, 0, 4, 0); // Задержка отправки на 4 секунды
+    NotificationCenter.ScheduleNotification(Notification); // отправка уведомления в компонент
+  finally
+    Notification.DisposeOf; // очистка переменной при возникновении ошибки
+  end;
 end;
 
 end.
